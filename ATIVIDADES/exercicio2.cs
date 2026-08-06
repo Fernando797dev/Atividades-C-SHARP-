b120c5532828aa0c7b41328dedbd16fae106390a
Fernando797dev <fernando.mai.dubiella@gmail.com>
2026-08-05 21:49:02 -0300

    Create currency conversion program in exercicio2.cs
    Implement basic structure for currency conversion program.

---
+++ ATIVIDADES/exercicio2.cs
@@ -0,0 +1,18 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+
+		
+	const double dolar = 5.14;
+	double real;
+	double conversorReal = real * dolar;
+	
+	Console.WriteLine("Escreva o número que deseja ser convertido");
+	
+
+		
+	}
+}
