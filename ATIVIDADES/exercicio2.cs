5ca48b2a681f4299dcc24ae4b7707e87fcb3ade9
Fernando797dev <fernando.mai.dubiella@gmail.com>
2026-08-06 18:54:40 -0300

    Implement dollar to real conversion program

--- ATIVIDADES/exercicio2.cs
+++ ATIVIDADES/exercicio2.cs
@@ -1,3 +1,9 @@
+//2. Faça um programa que:
+//● Leia a cotação do dólar
+//● Leia um valor em dólares
+//● Converta esse valor para Real
+//● Mostre o resultado
+
 using System;
 					
 public class Program
@@ -5,14 +11,17 @@ public class Program
 	public static void Main()
 	{
 
-		
-	const double dolar = 5.14;
-	double real;
-	double conversorReal = real * dolar;
-	
-	Console.WriteLine("Escreva o número que deseja ser convertido");
 	
 
+	
+	Console.WriteLine("Escreva a cotação do dolar");
+	double cotacaoDolar = double.Parse(Console.ReadLine());
+	
+	Console.WriteLine("Escreva o valor em dólares que deseja ser convertido para real");
+	double dolar = double.Parse(Console.ReadLine());
+	
+	double convertidoDolar = cotacaoDolar * dolar;
 		
+	Console.WriteLine("Esse é o seu resultado: " + convertidoDolar);
 	}
 }
